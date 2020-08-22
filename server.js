67fc0b796255b89300e448d252654219ac48e537
Camilo Lopez <johan.lopez@bluetab.net>
2020-08-22 17:02:09 -0500

    General error handler middleware added and cors dependency added

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const locationRoutes = require('./routes/locationRoutes');
 
 const SERVER_PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 locationRoutes(app);
+
+/**
+ * General error handler middleware.
+ */
+app.use((error, _, res, next) => {
+    if (error) {
+        console.log(err);
+        res.json(500).send({ message: 'Internal error' });
+    } else {
+        next();
+    }
+});
 
 /**
  * Put the server listening to requests on port 3000.
